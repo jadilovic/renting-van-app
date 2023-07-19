9f67244117e2bfd335c3f5d0785929876842e451
jadilovic <j.adilovic@gmail.com>
2023-07-19 06:56:53 +0200

    created mongodb demo

--- backend/demo.js
+++ backend/demo.js
@@ -10,13 +10,16 @@ async function main() {
 
 	try {
 		await client.connect();
+
 		// await listDatabases(client);
+
 		// await createListing(client, {
-		// 	name: 'Bihac App',
+		// 	name: 'Bosanski Novi App',
 		// 	description: 'Very big app',
 		// 	bedrooms: 5,
 		// 	bathrooms: 3,
 		// });
+
 		// await createMultipleListings(client, [
 		// 	{
 		// 		name: 'Bihac App',
@@ -43,12 +46,33 @@ async function main() {
 		// 		bathrooms: 3,
 		// 	},
 		// ]);
+
 		// await findOneListingByName(client, 'Bihac App');
-		await findListingsWithMinimumBedroomsBathroomsAndMostRecentReviews(client, {
-			minimumNumberOfBedrooms: 4,
-			minimumNumberOfBathrooms: 2,
-			maximumNumberOfResults: 5,
-		});
+
+		// await findListingsWithMinimumBedroomsBathroomsAndMostRecentReviews(client, {
+		// 	minimumNumberOfBedrooms: 4,
+		// 	minimumNumberOfBathrooms: 2,
+		// 	maximumNumberOfResults: 5,
+		// });
+
+		// await updateListingByName(client, 'Bihac App', {
+		// 	bedrooms: 1,
+		// 	bathrooms: 8,
+		// });
+
+		// await upsertListingByName(client, 'Bihac Novi App', {
+		// 	name: 'Bihac Novi App',
+		// 	bedrooms: 336,
+		// 	bathrooms: 336,
+		// });
+
+		// await updateAllListingsToHavePropertyType(client);
+
+		// await deleteListingByName(client, 'Bihac App');
+
+		// await updateAllBihacListings(client);
+
+		// await deleteListingsScrapedBeforeDate(client, new Date('2019-02-16'));
 	} catch (error) {
 		console.log(error);
 	} finally {
@@ -58,12 +82,57 @@ async function main() {
 
 main().catch((error) => console.log(error));
 
+async function deleteListingsScrapedBeforeDate(client, date) {
+	const result = await client
+		.db('sample_airbnb')
+		.collection('listingsAndReviews')
+		.deleteMany({ last_scraped: { $lt: date } });
+
+	console.log(result);
+	console.log(result.deletedCount);
+}
+
+async function deleteListingByName(client, listingName) {
+	const result = await client
+		.db('sample_airbnb')
+		.collection('listingsAndReviews')
+		.deleteOne({ name: listingName });
+
+	console.log(result);
+	console.log(result.deletedCount);
+}
+
+async function updateAllBihacListings(client) {
+	const result = await client
+		.db('sample_airbnb')
+		.collection('listingsAndReviews')
+		.updateMany({ name: 'Bihac App' }, { $set: { canton: 'USK' } });
+
+	console.log(result);
+	console.log(result.matchedCount);
+	console.log(result.modifiedCount);
+}
+
+async function updateAllListingsToHavePropertyType(client) {
+	const result = await client
+		.db('sample_airbnb')
+		.collection('listingsAndReviews')
+		.updateMany(
+			{ property_type: { $exists: false } },
+			{ $set: { property_type: 'Unknown' } }
+		);
+
+	console.log(result.matchedCount);
+	console.log(result.modifiedCount);
+}
+
 async function createListing(client, newListing) {
 	const result = await client
 		.db('sample_airbnb')
 		.collection('listingsAndReviews')
 		.insertOne(newListing);
 
+	console.log(result);
 	console.log(`New listing created with inserted ID: ${result.insertedId}`);
 }
 
@@ -113,6 +182,35 @@ async function findListingsWithMinimumBedroomsBathroomsAndMostRecentReviews(
 
 	if (results.length > 0) {
 		results.forEach((item) => console.log(`Name: ${item.name}`));
+	}
+}
+
+async function updateListingByName(client, nameOfListing, updatedListing) {
+	const result = await client
+		.db('sample_airbnb')
+		.collection('listingsAndReviews')
+		.updateOne({ name: nameOfListing }, { $set: updatedListing });
+
+	console.log(result.matchedCount);
+	console.log(result.modifiedCount);
+}
+
+async function upsertListingByName(client, nameOfListing, updatedListing) {
+	const result = await client
+		.db('sample_airbnb')
+		.collection('listingsAndReviews')
+		.updateOne(
+			{ name: nameOfListing },
+			{ $set: updatedListing },
+			{ upsert: true }
+		);
+
+	console.log(result.matchedCount);
+
+	if (result.upsertedCount > 0) {
+		console.log('One new document created with id ' + result.upsertedId);
+	} else {
+		console.log('Document was updated ' + result.modifiedCount);
 	}
 }
 
